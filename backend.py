0a321ae5b5ef4dfd519a53723648ed1ee2f2e465
AJ McMartin <amcmartin10@gmail.com>
2020-11-08 11:12:13 -0500

    changed the logging to a database and edited the log workout to add to the database

--- backend.py
+++ backend.py
@@ -1,12 +1,12 @@
-import csv
-import datetime
+import sqlite3
 from datetime import date
+import time
 import calendar
 
 
 class Workout:
     def __init__(self, category):
-        self.today = date.today()
+        self.today = int(time.time())
         self.DOTW = calendar.day_name[date.today().weekday()]
         self.category = category
         self.exercises = []
@@ -18,18 +18,22 @@ class Workout:
         return self.category
 
     def add_exercise(self,exercise):
-         self.exercises.append(exercise)
+        self.exercises.append(exercise)
 
     def log_exercises(self):
-        with open('data.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            for exercise in self.exercises:
-                writer.writerow([self.today,self.DOTW,self.category,exercise.name,exercise.sets,exercise.reps])
+        """Logs all information about the exercise into the data base"""
+        conn = sqlite3.connect("workout.db")
+        cursor = conn.cursor()
+        for exercise in self.exercises:
+            cursor.execute("INSERT INTO workout VALUES (?, ?, ?, ?, ?, ?)",
+                           (self.today, self.DOTW, self.category,exercise.name,exercise.sets,exercise.reps))
+        conn.commit()
+        conn.close()
 
 
-    def print_exercises(self):
-        for i in (self.exercises):
-            print(i.name)
+def print_exercises(self):
+    for i in (self.exercises):
+        print(i.name)
 
 
 class Exercise:
@@ -59,5 +63,21 @@ if __name__ == "__main__":
     exercise2 = Exercise("bent over rows", 4,10)
     workout.add_exercise(exercise1)
     workout.add_exercise(exercise2)
-    workout.log_exercises()
-
+    # workout.log_exercises()
+    # conn = sqlite3.connect("workout.db")
+    # cursor = conn.cursor()
+    # cursor.execute("""CREATE TABLE workout (
+    #     Date integer,
+    #     DayOfTheWeek text,
+    #     Category text,
+    #     exercise text,
+    #     Sets integer,
+    #     Reps integer
+    #     )""")
+    conn = sqlite3.connect("workout.db")
+    cursor = conn.cursor()
+    # cursor.execute("INSERT INTO workout VALUES (1576280665, 'Saturday', 'Back', 'Deadlift', 5, 5)")
+    cursor.execute("SELECT * FROM workout WHERE DayOfTheWeek='Saturday'")
+    print(cursor.fetchall())
+    # conn.commit()
+    conn.close()
